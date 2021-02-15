6ca2bc20ee9391d4460dd1efd2c6db98ebba41da
AmarelleDiArgento <ewige.qual@gmail.com>
2021-02-15 12:35:00 -0500

    ajustes de environment

--- src/environments/environment.stag.ts
+++ src/environments/environment.stag.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'http://thegraphdatatest.eliteflower.co/',
+  api_url: 'http://thegraphdatatest.eliteflower.co',
   api_receta_url: 'http://10.50.1.120:4000'
 };
